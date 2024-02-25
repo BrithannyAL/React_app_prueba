1b3057a251e40e1c1f87ea5290f93efffe82bb9f
Brithanny Arguello <49217000+BrithannyAL@users.noreply.github.com>
2024-02-25 10:22:13 -0600

    cambios pequenos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Libros from "./componentsAPI/Libros";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./componentsAPI/Navbar";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
+      <Navbar />
+
       <Libros />
-    </div>
+    </BrowserRouter>
   );
 }
 
